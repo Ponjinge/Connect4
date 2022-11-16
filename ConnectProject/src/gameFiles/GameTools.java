fdc2696c1ef2c362063ace1074a6e8d6b4170bb8
Ponjinge <34633743+Ponjinge@users.noreply.github.com>
2022-11-16 15:40:54 +0100

    Added game pieces and DripDown function

--- ConnectProject/src/gameFiles/GameTools.java
+++ ConnectProject/src/gameFiles/GameTools.java
@@ -1,6 +1,20 @@
 package gameFiles;
 
 public class GameTools {
+	
+	public static int[][] dripDown( int[][] tab, int column , int turn) throws InterruptedException{
+	    int i=0;
+	    tab[i][column]=turn;
+	    while(tab[i+1][column]==0){
+	        tab[i+1][column]=turn;
+	        tab[i][column]=0;
+	        Thread.sleep(200);
+	    }
+	    
+	    return tab;
+	}
+	
+	
 	public static int[][] tableReset(int[][] tab , int column){
 	    int i=0;
 		while(tab[i][column]==0){
@@ -18,4 +32,5 @@ public class GameTools {
 	        return false;
 	    }
 	}
+	
 }
