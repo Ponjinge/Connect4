27ec6b1e14a9b1ba4f29603a81ec65700993e1c8
Ponjinge <34633743+Ponjinge@users.noreply.github.com>
2022-11-18 04:15:12 +0100

    UI Improvement

--- ConnectProject/src/gameFiles/GameTools.java
+++ ConnectProject/src/gameFiles/GameTools.java
@@ -99,6 +99,7 @@ public class GameTools {
 	
 	public static void tabDisplay(int[][]tab) {
 		//This displays our board
+		System.out.println("| 0 | 1 | 2 | 3 | 4 | 5 | 6 |");
 		for(int i=0;i<tab.length;i++) {
 			System.out.print("| ");
 			for(int j=0; j<tab[0].length;j++) {
